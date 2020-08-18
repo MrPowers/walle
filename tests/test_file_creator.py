62240851ebb7bed351e3c929675ec13806c9b8bb
MrPowers <matthewkevinpowers@gmail.com>
2020-08-17 20:07:48 -0500

    Add more compression algos for CSV files

--- tests/test_file_creator.py
+++ tests/test_file_creator.py
@@ -6,6 +6,18 @@ def test_write_gzipped_csv():
     write_gzipped_csv()
 
 
+def test_write_bz2_csv():
+    write_bz2_csv()
+
+
+def test_write_zip_csv():
+    write_zip_csv()
+
+
+# def test_write_xz_csv():
+    # write_xz_csv()
+
+
 def test_write_parquet():
     write_parquet()
 
@@ -20,3 +32,5 @@ def test_write_gzip_parquet():
 
 def test_write_brotli_parquet():
     write_brotli_parquet()
+
+
--- walle/file_creator.py
+++ walle/file_creator.py
@@ -1,26 +1,46 @@
 import pandas as pd
+import os
+
+
+os.makedirs("tmp/csv", exist_ok=True)
+os.makedirs("tmp/parquet", exist_ok=True)
 
 
 def write_gzipped_csv():
     df = pd.read_csv('data/writing_data1.csv')
-    df.to_csv('tmp/writing_data1.csv.gz', compression='gzip')
+    df.to_csv('tmp/csv/writing_data1.csv.gz', compression='gzip')
+
+
+def write_bz2_csv():
+    df = pd.read_csv('data/writing_data1.csv')
+    df.to_csv('tmp/csv/writing_data1.csv.bz2', compression='bz2')
+
+
+def write_zip_csv():
+    df = pd.read_csv('data/writing_data1.csv')
+    df.to_csv('tmp/csv/writing_data1.csv.zip', compression='zip')
+
+
+# def write_xz_csv():
+    # df = pd.read_csv('data/writing_data1.csv')
+    # df.to_csv('tmp/csv/writing_data1.csv.xz', compression='xz')
 
 
 def write_parquet():
     df = pd.read_csv('data/writing_data1.csv')
-    df.to_parquet('tmp/writing_data1.parquet', compression = None)
+    df.to_parquet('tmp/parquet/writing_data1.parquet', compression = None)
 
 
 def write_snappy_parquet():
     df = pd.read_csv('data/writing_data1.csv')
-    df.to_parquet('tmp/writing_data1.snappy.parquet', compression = 'snappy')
+    df.to_parquet('tmp/parquet/writing_data1.snappy.parquet', compression = 'snappy')
 
 
 def write_gzip_parquet():
     df = pd.read_csv('data/writing_data1.csv')
-    df.to_parquet('tmp/writing_data1.gzip.parquet', compression = 'gzip')
+    df.to_parquet('tmp/parquet/writing_data1.gzip.parquet', compression = 'gzip')
 
 
 def write_brotli_parquet():
     df = pd.read_csv('data/writing_data1.csv')
-    df.to_parquet('tmp/writing_data1.brotli.parquet', compression = 'brotli')
+    df.to_parquet('tmp/parquet/writing_data1.brotli.parquet', compression = 'brotli')
